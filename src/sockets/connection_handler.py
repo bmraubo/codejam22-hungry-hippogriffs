b241f37c465cea32714c5f7d042e0a140e728da4
Bartosz Raubo <braubo@8thlight.com>
2022-07-24 15:13:56 +0100

    remove process_incoming_connection loop and fix bug where I forgot to rename method call.

--- src/sockets/connection_handler.py
+++ src/sockets/connection_handler.py
@@ -6,16 +6,15 @@ class ConnectionHandler:
         self.socket = socket
 
     async def get_request(self):
-        return await self.wait_for_connection()
+        return await self.process_incoming_connection()
 
     async def send_response(self, response: dict):
         await self.socket.send_text(json.dumps(response))
 
     async def process_incoming_connection(self) -> str:
         await self.socket.accept()
-        while True:
-            request = await self.socket.receive_text()
-            return self.parse_request(request)
+        request = await self.socket.receive_text()
+        return self.parse_request(request)
 
     def parse_request(self, request: str) -> dict:
         return json.loads(request)
